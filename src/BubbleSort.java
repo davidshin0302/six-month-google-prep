5339fd60abac0792102d9fa9cf5b820cebfcf176
github username <github email address>
2025-04-02 12:05:41 -0400

    bublle sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,17 @@
+public class BubbleSort {
+    public void sortBubble(String[] arr) {
+        if (arr == null || arr.length < 0) {
+            throw new IllegalArgumentException("Invalid Agrumnet");
+        }
+
+        if (arr.length == 1) {
+            System.out.println("There is nothing to sort");
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 1; j < arr.length - 1; j++){
+                if( )
+            }
+        }
+    }
+}
