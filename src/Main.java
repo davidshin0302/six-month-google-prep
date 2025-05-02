fea1297092ccfba15d2c617582ef82350cd40c04
github username <github email address>
2025-05-02 15:06:49 -0400

    practicing merge sort algorithm.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
         int[] arr =  {5,2,4,1,6};
 
-        System.out.println(Arrays.toString(insertionSort.practiceInsertion(arr)));
+//        System.out.println(Arrays.toString(insertionSort.practiceInsertion(arr)));
+
+        MergeSort.mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
