cc52a59b79ff3473ab98fcc545ef3ef0d88de468
github username <github email address>
2025-05-05 12:32:27 -0400

    practicing quick sort algorithm.

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         InsertionSort insertionSort = new InsertionSort();
 
-        int[] arr =  {5,2,4,1,6};
+        int[] arr = {5, 2, 4, 1, 6, 7};
 
 //        System.out.println(Arrays.toString(insertionSort.practiceInsertion(arr)));
 
-        MergeSort.mergeSort(arr);
+//        MergeSort.mergeSort(arr);
+        QuickSort.quickSort(arr, 0, 5);
         System.out.println(Arrays.toString(arr));
     }
 }
