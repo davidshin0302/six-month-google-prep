51506fa1760c09f0abfc04d61af0736926ac3ded
github username <github email address>
2025-05-06 11:36:37 -0400

    practiced depth first search algo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import searchAlgorithm.BinarySearch;
 import searchAlgorithm.BreadthFirstSearch;
+import searchAlgorithm.DepthFirstSearch;
 import sortAlgorithm.InsertionSort;
 import sortAlgorithm.QuickSort;
 
@@ -9,14 +10,22 @@ public class Main {
     public static void main(String[] args) {
         InsertionSort insertionSort = new InsertionSort();
         BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch(0, 5);
+        DepthFirstSearch depthFirstSearch = new DepthFirstSearch(0);
 
         int[] arr = {5, 2, 4, 1, 6, 7};
         int[] sortedArr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
 
+        System.out.println("================");
         System.out.println(BinarySearch.binarySearch(sortedArr, 23));
+
+        System.out.println("================");
         QuickSort.quickSort(arr, 0, 5);
         System.out.println(Arrays.toString(arr));
 
+        System.out.println("================");
         breadthFirstSearch.printResult();
+
+        System.out.println("================");
+        depthFirstSearch.printOutput();
     }
 }
--- src/searchAlgorithm/DepthFirstSearch.java
+++ src/searchAlgorithm/DepthFirstSearch.java
@@ -1,4 +1,47 @@
 package searchAlgorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class DepthFirstSearch {
+    private int currentNode;
+    private List<List<Integer>> adj;
+    private boolean[] visited;
+
+    public DepthFirstSearch(int currentNode) {
+        this.currentNode = currentNode;
+        this.visited = new boolean[]{false, false, false, false, false, false};
+        int numberOfNode = 6;
+        List<List<Integer>> adj = new ArrayList<>(numberOfNode);
+
+        for (int i = 0; i < numberOfNode; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        adj.get(0).addAll(Arrays.asList(1, 2));
+        adj.get(1).add(3);
+        adj.get(2).add(4);
+        adj.get(3).add(5);
+        adj.get(4).add(5);
+
+        this.adj = adj;
+    }
+
+    public void depthFirstSearch(int currentNode, List<List<Integer>> adj, boolean[] visited) {
+        visited[currentNode] = true;
+        System.out.println(currentNode + " ");
+
+        for (int node : adj.get(currentNode)) {
+            if (!visited[node]) {
+                depthFirstSearch(node, adj, visited);
+            }
+        }
+    }
+
+    public void printOutput() {
+        depthFirstSearch(currentNode, adj, visited);
+        System.out.println(Arrays.toString(visited));
+    }
+
 }
