8c1b0b827232ad23d6316171e590baada7a0d333
github username <david.shin0302@gmail.com>
2025-05-27 15:33:11 -0400

    working to DSA binary tree lab.

---
+++ src/trees/TreeNode.java
@@ -0,0 +1,13 @@
+package trees;
+
+public class TreeNode<E> {
+    E data;
+    TreeNode<E> left;
+    TreeNode<E> right;
+
+    public TreeNode(E data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
