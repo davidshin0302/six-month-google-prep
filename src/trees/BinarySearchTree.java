97010cc7b1a739a23390bcfcd934933a90bea989
github username <david.shin0302@gmail.com>
2025-05-29 11:58:44 -0400

    working on implementing delete method on binary search tree.

---
+++ src/trees/BinarySearchTree.java
@@ -0,0 +1,86 @@
+package trees;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+    TreeNode<E> root;
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public void insert(E element) {
+        this.root = insert(root, element);
+        System.out.println(root.data);
+    }
+
+    private TreeNode<E> insert(TreeNode<E> current, E element) {
+        if (current == null) {
+            current = new TreeNode<>(element);
+            return current;
+        }
+
+        if (element.compareTo(current.data) < 0) {
+            current.left = insert(current.left, element);
+        } else if (element.compareTo(current.data) > 0 || element.compareTo(current.data) == 0) {
+            current.right = insert(current.right, element);
+        }
+        return current;
+    }
+
+    public void search(E element) {
+        boolean result = search(root, element);
+
+        if (!result) {
+            System.out.println("The element doesn't not exit");
+        } else {
+            System.out.println("Found the element from the storage: " + element);
+        }
+    }
+
+    private boolean search(TreeNode<E> current, E element) {
+        if (current == null) {
+            return false;
+        } else if (element.compareTo(current.data) == 0) {
+            return true;
+        }
+
+        if (element.compareTo(current.data) < 0) {
+            return search(current.left, element);
+        } else if (element.compareTo(current.data) > 0) {
+            return search(current.right, element);
+        } else {
+            return search(current.right, element);
+        }
+    }
+
+    public void delete(E element) {
+
+    }
+
+    private TreeNode<E> delete(TreeNode<E> current, E element) {
+        if (current == null) {
+            return null;
+        }
+
+        int cmp = element.compareTo(current.data);
+
+        if (cmp < 0) {
+            delete(current.left, element);
+        } else if (cmp > 0) {
+            current.right = delete(current.right, element);
+        } else {
+            current.right = delete(current.right, element);
+        }
+
+        if (cmp == 0) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            if (current.left == null && current.right != null) {
+                return current.left;
+            }
+            if (current.right == null && current.left != null) {
+                return current.right;
+            }
+
+        }
+    }
