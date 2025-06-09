4c2ce4113c7976c7a482d7898ce20c93bfb715be
github username <david.shin0302@gmail.com>
2025-06-09 12:58:16 -0400

    working on solving binary Tree traversal challenge try-2.

--- src/leetCodeChallenges/stack/BinaryTreeTraversal.java
+++ src/leetCodeChallenges/stack/BinaryTreeTraversal.java
@@ -1,24 +1,29 @@
 package leetCodeChallenges.stack;
 
-import trees.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/binary-tree-level-order-traversal/
  */
 public class BinaryTreeTraversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root == null){
-            return null;
+    public List<List<Integer>> levelOrder(StackTreeNode root) {
+        List<List<Integer>> output = new ArrayList<>();
+        Queue<StackTreeNode> queue = new ArrayDeque<>();
+
+        if (root == null) {
+            return output;
         }
 
-        if(root.left != null){
-            levelOrder(root.left);
-        }else if(root.right != null){
-            levelOrder(root.right);
-        }else{
-            return null;
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            while()
         }
     }
+
+
 }
